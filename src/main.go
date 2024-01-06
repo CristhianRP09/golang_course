aab9f5fa83c12cf3dc3b4547f5f4e9f4622f4759
Cristhian Rodriguez <cristhianr.0912@gmail.com>
2024-01-05 22:44:17 -0500

    Part_3

--- src/main.go
+++ src/main.go
@@ -1,58 +1,55 @@
 package main
 
-import "fmt"
-
-func normalFunction(msg string) {
-	fmt.Printf(">>>>>>>>>>> Hello world %s\n", msg)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func duplicate(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, 2 * a
-}
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
-	helloMessage := "Hello"
-	worldMessage := "World"
+	// Conditional statements
+	value1 := 1
+	value2 := 3
 
-	// Println
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
+	if value1 == 1 {
+		fmt.Println("Equals to 1")
+	} else {
+		fmt.Println("Not equal to 1")
+	}
 
-	// Printf
-	name := "Cristhian"
-	age := 27
-	fmt.Printf("My name is %s and I am %d years old\n", name, age)
-	fmt.Printf("My name is %v and I am %v years old\n", name, age)
+	// AND operator
+	if value1 == 1 && value2 == 2 {
+		fmt.Println("TRUE")
+	}
 
-	// Sprintf
-	message := fmt.Sprintf("My name is %s and I am %d years old", name, age)
-	fmt.Println(message)
+	// OR operator
+	if value1 == 0 || value2 == 3 {
+		fmt.Println("TRUE")
+	}
 
-	// Data type
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-	fmt.Printf("name: %T\n", name)
-
-	// Functions
-	normalFunction("1")
-	normalFunction("2")
-	tripleArgument(1, 2, "Hello!")
-
-	value := duplicate(3)
+	//  Parsing text into numbers
+	value, error := strconv.Atoi("53")
+	if error != nil {
+		log.Fatal(error)
+	}
 	fmt.Println("Value:", value)
 
-	value1, value2 := doubleReturn(2)
-	fmt.Println("Value1:", value1)
-	fmt.Println("Value2:", value2)
+	// Conditional for loop
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	// Just using the first returned value
-	v1, _ := doubleReturn(5) // _ means ignoring the 2nd value in this case
-	fmt.Println("v1:", v1)
+	// For while loop
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever loop
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
