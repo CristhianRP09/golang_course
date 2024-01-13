7a81e62e6c9f5ce8cd4fb37a51c83253e21b75ed
Cristhian Rodriguez <cristhianr.0912@gmail.com>
2024-01-12 22:43:05 -0500

    Part_5

--- src/main.go
+++ src/main.go
@@ -1,52 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(text string) {
+	var textReverse string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if strings.ToLower(text) == strings.ToLower(textReverse) {
+		fmt.Println("Is Palindrome!")
+	} else {
+		fmt.Println("Is Not Palindrome!")
+	}
+}
 
 func main() {
-	module := 4 % 2
+	// Arrays
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array)
+	fmt.Println(len(array))
+	fmt.Println(cap(array))
 
-	switch module {
-	case 0:
-		fmt.Println("Is even!")
-	default:
-		fmt.Println("Is odd!")
+	// Slices
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+	// Slice methods
+	fmt.Println(slice[0])   // Shows the 1st element of slice
+	fmt.Println(slice[:3])  // Shows the from 0 to 2nd elements of slice
+	fmt.Println(slice[2:4]) // Shows the from 2 to 3rd element (without 4th) of slice
+	fmt.Println(slice[4:])  // Shows elements from 4th onwards
+
+	// Append Method
+	slice = append(slice, 7) // Add element 7 to slice
+	fmt.Println(slice)
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...) // Add element the slice {9, 9, 10} to previous slice
+	fmt.Println(slice)
+
+	// Range method
+	otherSlice := []string{"Hey", "How", "are", "you"}
+
+	for i, value := range otherSlice {
+		fmt.Println(i, value)
 	}
 
-	// In a short way
-	switch module := 4 % 2; module {
-	case 0:
-		fmt.Println("Is even!")
-	default:
-		fmt.Println("Is odd!")
-	}
-
-	// Switch sentence without condition
-	value := 120
-	switch {
-	case value > 100:
-		fmt.Println("Greater than 100")
-	case value < 0:
-		fmt.Println("Less than 0")
-	default:
-		fmt.Println("Within range from 0 to 100")
-	}
-
-	// defer keyword
-	fmt.Println("Hello ... {")
-	defer fmt.Println("... World }")
-
-	// continue and break keywords
-	for i := 0; i < 10; i++ {
-		if i != 2 {
-			fmt.Println(i)
-		} else {
-			fmt.Println("It's 2")
-			continue
-		}
-
-		if i == 8 {
-			fmt.Println("It's 8... END")
-			break
-		}
-	}
+	// Exercise
+	isPalindrome("ama")         // true
+	isPalindrome("amor a roma") // true
+	isPalindrome("casas")       // false
+	isPalindrome("amA")         // true
+	isPalindrome("aMoR A ROmA") // true
 }
